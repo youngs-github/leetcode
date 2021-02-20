e9f8814d9b553e8ef70dfda022caffb1a9c35398
yangqingcheng <yangqingcheng@kedacom.com>
2021-02-20 17:41:39 +0800

    假期最后一天

---
+++ 42.接雨水.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=42 lang=javascript
+ *
+ * [42] 接雨水
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+	if (height.length < 2) return 0;
+	
+};
+// @lc code=end
+
